4991a8ce1a5d89b407b4c9434c04a324b1752a91
Sagar Narsingani <sagarhn2411@gmail.com>
2023-04-22 20:31:15 +0530

    Change Backend Port

--- src/backend/app.js
+++ src/backend/app.js
@@ -5,11 +5,11 @@ require('./db');
 const {user} = require('./routes/user');
 
 const app = express();
-
+const port = process.env.PORT || 3000
 app.use(cors());
 app.use(express.urlencoded({extended: false}));
 app.use('/user', user);
 
-app.listen(3000, function () {
-    console.log('Server is up and running on port 3000!');
+app.listen(port, function () {
+    console.log(`Server is up and running on port ${port}!`);
 });
